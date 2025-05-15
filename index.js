371287ef7192fa343548dc592a0fd8250bc8a51b
jobayer58 <jobayerafnan770@gmail.com>
2025-05-16 04:44:51 +0600

    install jwt and and create user verify token system,andsave user data to database,and get from database and show data in ui

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -32,11 +33,64 @@ async function run() {
     const userDataCollection = client.db('Matrimony').collection('users')
     const favoriteBioDataCollection = client.db('Matrimony').collection('favoriteBioData')
 
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+      res.send({ token });
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userDataCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
     // user related apis
     // get/show all user data in manage user page
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      console.log(req.headers);
       const result = await userDataCollection.find().toArray()
       res.send(result)
+    })
+
+    // get admin user data from the database
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userDataCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
     })
 
     // send user data to database(create/add)
@@ -52,6 +106,22 @@ async function run() {
       res.send(result)
     })
 
+    // user make admin api
+    app.patch('/users/admin/:id', verifyAdmin, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userDataCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+
+
+    // bioData related api
     // get/show all BioData  data in home page
     app.get('/matchesBio', async (req, res) => {
       const result = await bioDataCollection.find().toArray()
@@ -67,11 +137,39 @@ async function run() {
     })
 
     // favorite BioData Collection
+
+    // get favorite list data from database
+    app.get('/favorite', async (req, res) => {
+      const email = req.query.email
+      const query = { email: email }
+      const result = await favoriteBioDataCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // send favorite data to database
     app.post('/favorite', async (req, res) => {
       const favoriteItem = req.body;
+      const { email, bioId } = favoriteItem;
+
+      const query = { email, bioId };
+      const existing = await favoriteBioDataCollection.findOne(query);
+
+      if (existing) {
+        return res.send({ message: 'exists' });
+      }
+
       const result = await favoriteBioDataCollection.insertOne(favoriteItem);
       res.send(result);
     });
+
+
+    // favorite BioData item delete
+    app.delete('/favorite/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await favoriteBioDataCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
